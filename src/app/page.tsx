26aaa94ef3788401d17cafcb84c08bfec7561a9f
Vegildo <leobss12@gmail.com>
2025-03-21 13:33:32 -0300

    Feat: Adicionando loading spinner na exibição de posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 import { useState, useEffect } from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 import PostCard from './components/PostCard';
 import Navbar from './components/Navbar';
 import { Hero } from './components/Hero';
 import GlobalStyle from "@/app/componentStyles/globalStyles";
+
+const LoadingSpinner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 300px; // Ajuste conforme necessário
+  svg {
+    width: 50px;
+    height: 50px;
+    animation: spin 1s linear infinite;
+  }
+
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 const Container = styled.div`
   padding: 20px;
@@ -51,10 +72,12 @@ interface Post {
 export default function Home() {
   const [search, setSearch] = useState<string>('');
   const [posts, setPosts] = useState<Post[]>([]); // Estado para armazenar os posts
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
+        setLoading(true); // Inicia o carregamento
         const response = await fetch('https://blog-posts-hori.onrender.com/post'); // Chamada à API
         const data = await response.json();
         const mappedPosts = data.map((post: any) => ({
@@ -67,6 +90,8 @@ export default function Home() {
         setPosts(mappedPosts); // Atualiza o estado com os dados mapeados
       } catch (error) {
         console.error('Erro ao buscar os posts:', error);
+      } finally {
+        setLoading(false); // Finaliza o carregamento, independentemente do sucesso ou erro
       }
     };
 
@@ -102,9 +127,18 @@ export default function Home() {
           </div>
 
           <Container>
-            {filteredPosts.map((post, index) => (
-              <PostCard key={post.id || index} post={post} /> // Usa o índice como fallback para a key
-            ))}
+            {loading ? (
+                <LoadingSpinner>
+                  <svg viewBox="0 0 50 50">
+                    <circle cx="25" cy="25" r="20" fill="none" stroke="#fff" strokeWidth="4" strokeDasharray="100" />
+                  </svg>
+                </LoadingSpinner>
+              ) : (
+                filteredPosts.map((post, index) => (
+                  <PostCard key={post.id || index} post={post} /> // Usa o índice como fallback para a key
+                ))
+              )
+            }
           </Container>
         </div>
       </MainContent>
